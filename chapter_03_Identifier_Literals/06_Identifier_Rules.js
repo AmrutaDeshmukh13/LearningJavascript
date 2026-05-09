94fce1ca2e92b15568f3f7589a29b788c4142373
Amruta <amrujagtap@gmail.com>
2026-05-09 13:16:47 +0530

    Add new JavaScript files: HotCode, JS Basics, and Identifier Rules

---
+++ chapter_03_Identifier_Literals/06_Identifier_Rules.js
@@ -0,0 +1,22 @@
+var $ = 10;
+var _a = 12;
+var p = 10;
+
+var ab123 = 34;
+
+// var 123 = 123;
+
+var Name = "pramod";
+var name = "Amit";
+//var pramod dutta = "hello";
+var pramod_dutta = "hello";
+var pramod$dutta = "hello";
+var pramodu1232 = "hello";
+
+/*• Must begin with a letter (a–z, A–Z), underscore (_), or dollar sign ($).
+• Subsequent characters may also include digits (0–9).
+• Cannot start with a digit.
+• Cannot be a reserved keyword.
+• Are case-sensitive.
+• May contain Unicode letters and Unicode escape sequences.
+• Cannot contain spaces, hyphens, or special characters (except _ and $).*/
